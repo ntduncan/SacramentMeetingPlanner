c4f005e12a671d9f3561dcfac00d32d613485bb1
ntduncan <nateduncan0@.com>
2022-07-09 01:19:01 -0600

    updated index page to be unique

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,29 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using SacramentMeetingPlanner.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace SacramentMeetingPlanner.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly SacramentMeetingPlannerContext _context;
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, SacramentMeetingPlannerContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var sacramentMeetingPlannerContext =  _context.SacramentMeetingProgram.Include(s => s.ClosingHymn).Include(s => s.IntermediateHymn).Include(s => s.OpeningHymn).Include(s => s.SacramentHymn);
+        
+        ViewData["Programs"] = sacramentMeetingPlannerContext.ToListAsync();
+        return View(await sacramentMeetingPlannerContext.ToListAsync());
     }
 
     public IActionResult Privacy()
